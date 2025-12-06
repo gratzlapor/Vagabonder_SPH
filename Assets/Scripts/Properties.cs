5e13a6c675ec4ab6200e50b95942b407a668673b
gratzlapor <84022646+gratzlapor@users.noreply.github.com>
2025-12-06 11:40:07 +0100

    Calculate density

---
+++ Assets/Scripts/Properties.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class Properties : MonoBehaviour
+{
+    public float mass = 18.015f;
+    public float velocity;
+    public float density;
+    public float radius;
+}
--- Assets/Scripts/SPH.cs
+++ Assets/Scripts/SPH.cs
@@ -1,19 +1,23 @@
 using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
+
+//1.Calculate density
 
 public class SPH : MonoBehaviour
 {
-    public List<GameObject> particles;
+    public List<GameObject> particles = new List<GameObject>();
     public float gizmoBoxSize = 5;
+    public float radius = 1;
+    public float particleCount = 3;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        for (int i = 0; i < 2; i++)
+        for (int i = 0; i < particleCount; i++)
         {
             GameObject particle = new GameObject("Particle " + i);
-            particle.transform.position = new Vector3 (0, 0, i);
+            particle.transform.position = new Vector3(0, 0, i*2);
+            particle.AddComponent<Properties>();
             particles.Add(particle);
         }
     }
@@ -21,8 +25,8 @@ public class SPH : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        Gravity();
         StayInBound();
+        CalculateDensity();
     }
 
     void OnDrawGizmos()
@@ -31,22 +35,31 @@ public class SPH : MonoBehaviour
         Gizmos.DrawWireCube(Vector3.zero, new Vector3(gizmoBoxSize, gizmoBoxSize, gizmoBoxSize));
     }
 
-    void Gravity()
+    void StayInBound()
     {
-        foreach (GameObject particle in particles)
+        foreach (var particle in particles)
         {
-            particle.transform.position += new Vector3(0f, -9.81f, 0f) * Time.deltaTime;
+            if (particle.transform.position.y < -gizmoBoxSize / 2)
+            {
+                particle.transform.position = new Vector3(particle.transform.position.x, -gizmoBoxSize / 2, particle.transform.position.z);
+            }
         }
     }
 
-    void StayInBound()
+    void CalculateDensity()
     {
-        foreach(GameObject particle in particles)
+        for (int i = 0; i < particleCount; i++)
         {
-            if(particle.transform.position.y < -gizmoBoxSize/2)
+            float allMass = 0;
+            for (int j = 0; j < particleCount; j++)
             {
-                particle.transform.position = new Vector3(particle.transform.position.x, -gizmoBoxSize/2, particle.transform.position.z);
+                float difference = Vector3.Distance(particles[i].transform.position, particles[j].transform.position);
+                if (difference < radius)
+                {
+                    allMass += particles[j].GetComponent<Properties>().mass * (315f / (64 * Mathf.PI * radius)) * Mathf.Pow(radius - Mathf.Pow(difference, 2), 3);
+                }
             }
+            particles[i].GetComponent<Properties>().density = allMass;
         }
     }
 }
