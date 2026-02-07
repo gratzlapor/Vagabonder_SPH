0b45fb83ba940c3ad91312652573cfd4cad62fb8
gratzlapor <84022646+gratzlapor@users.noreply.github.com>
2026-02-07 16:41:28 +0100

    Seperate arrays for boundaries

--- Assets/Scripts/GPU SPH/ComputeSetup.cs
+++ Assets/Scripts/GPU SPH/ComputeSetup.cs
@@ -1,5 +1,7 @@
+using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using System.Threading;
+using NUnit.Framework;
 using Unity.Mathematics;
 using UnityEngine;
 using static UnityEngine.ParticleSystem;
@@ -16,19 +18,31 @@ public struct Particle // 68bytes
     public float3 velocity;
     public float3 position;
 }
+
+[System.Serializable]
+[StructLayout(LayoutKind.Sequential, Size = 16)]
+public struct BoundaryParticle // 68bytes
+{
+    public float boundaryMass;
+    public float3 position;
+}
 public class ComputeSetup : MonoBehaviour
 {
-    ComputeBuffer buffer;
+    ComputeBuffer particleBuffer;
+    ComputeBuffer boundaryBuffer;
     public ComputeShader computeShader;
     int calcVariablesKernel;
     int calcForcesKernel;
     int calcPosition;
-    public Particle[] Particles; // 64 water particles + 52 wall particles
-    public GameObject[] renderedParticles;
+    public Particle[] Particles;
+    public Particle[] boundaryParticles;
+    GameObject[] renderedFluidParticles;
+    GameObject[] renderedBoundaryParticles;
     Vector3 wCountVector = new Vector3(8,6,9); // 8*6*8 = 432
-    int wCountInt;
-    int wallSize = 20; // 2168
-    int threads = 100; // Frissítsd az gpu oldalt is
+    public int wCountInt;
+    public int bCountInt; // 2168
+    int wallSize = 20;
+    int threads = 16; // Frissítsd az gpu oldalt is
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -36,38 +50,35 @@ public class ComputeSetup : MonoBehaviour
         ParticleSetup();
         RenderSetup();
         BufferAndDispatchSetup();
-        RenderParticles(Particles.Length);
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        computeShader.Dispatch(calcVariablesKernel, Particles.Length / threads, 1, 1);
-        computeShader.Dispatch(calcForcesKernel, Particles.Length / threads, 1, 1);
-        computeShader.Dispatch(calcPosition, Particles.Length / threads, 1, 1);
+        computeShader.Dispatch(calcVariablesKernel, (Particles.Length) / threads, 1, 1);
+        computeShader.Dispatch(calcForcesKernel, (Particles.Length) / threads, 1, 1);
+        computeShader.Dispatch(calcPosition, (Particles.Length) / threads, 1, 1);
 
-        buffer.GetData(Particles);
+        particleBuffer.GetData(Particles);
+        boundaryBuffer.GetData(boundaryParticles);
 
-        RenderParticles(wCountInt);
+        RenderParticlesUpdate();
+        RenderBoundaryParticlesUpdate();
 
-        for (int i = wCountInt; i < Particles.Length; i++)
-        {
-            Particles[i].position = renderedParticles[i].transform.position;
-        }
+        particleBuffer.SetData(Particles);
+        boundaryBuffer.SetData(boundaryParticles);
 
-        buffer.SetData(Particles);
     }
 
     private void OnDestroy()
     {
-        buffer.Dispose();
+        particleBuffer.Dispose();
+        boundaryBuffer.Dispose();
     }
 
     void ParticleSetup()
     {
-        wCountInt = (int)(wCountVector.x * wCountVector.y * wCountVector.z);
-        Particles = new Particle[wCountInt + 2168];
-        int counter = 0;
+        List<Particle> fluidList = new List<Particle>();
 
         // Water
         for (int i = 0; i < wCountVector.x; i++)
@@ -76,13 +87,17 @@ public class ComputeSetup : MonoBehaviour
             {
                 for (int k = 0; k < wCountVector.z; k++)
                 {
-                    Particles[counter] = new Particle();
-                    Particles[counter].position = new Vector3(transform.position.x+i, transform.position.y+j, transform.position.z+k);
-                    counter++;
+                    Particle p = new Particle();
+                    p.position = new Vector3(transform.position.x+i, transform.position.y+j, transform.position.z+k);
+                    fluidList.Add(p);
                 }
             }
         }
+        Particles = fluidList.ToArray();
+        wCountInt = Particles.Length;
 
+
+        List<Particle> boundaryList = new List<Particle>();
         //Walls
         for (int i = -10; i < wallSize - 10; i++)
         {
@@ -94,71 +109,89 @@ public class ComputeSetup : MonoBehaviour
                     {
                         k += 18;
                     }
-                    Particles[counter] = new Particle();
-                    Particles[counter].position = new Vector3(k, i, j);
-                    counter++;
+                    Particle p = new Particle();
+                    p.position = new Vector3(k, i, j);
+                    boundaryList.Add(p);
                 }
             }
         }
+        boundaryParticles = boundaryList.ToArray();
+        bCountInt = boundaryParticles.Length;
     }
 
     void RenderSetup()
     {
-        renderedParticles = new GameObject[Particles.Length];
+        renderedFluidParticles = new GameObject[Particles.Length];
+        renderedBoundaryParticles = new GameObject[boundaryParticles.Length];
         GameObject wallParent = GameObject.CreatePrimitive(PrimitiveType.Cube);
         wallParent.transform.position = new Vector3(10, 0, 10);
         wallParent.transform.localScale = new Vector3(20, 20, 20);
         wallParent.GetComponent<MeshRenderer>().enabled = false;
         for (int i = 0; i < Particles.Length; i++)
         {
-            if (i < wCountInt)
-            {
-                renderedParticles[i] = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-                renderedParticles[i].transform.localScale = new Vector3(1f, 1f, 1f);
-            }
-            else
-            {
-                renderedParticles[i] = new GameObject();
-                renderedParticles[i].transform.SetParent(wallParent.transform,false);
-            }
-
-            renderedParticles[i].name = "p_" + i;
+            renderedFluidParticles[i] = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            renderedFluidParticles[i].transform.localScale = new Vector3(1f, 1f, 1f);
+            renderedFluidParticles[i].name = "p_" + i;
+            renderedFluidParticles[i].transform.position = Particles[i].position;
+        }
+        for(int i = 0;i < boundaryParticles.Length;i++)
+        {
+            renderedBoundaryParticles[i] = new GameObject();
+            renderedBoundaryParticles[i].transform.SetParent(wallParent.transform, false);
+            renderedBoundaryParticles[i].name = "b_" + i;
+            renderedBoundaryParticles[i].transform.position = boundaryParticles[i].position;
         }
     }
 
     void BufferAndDispatchSetup()
     {
-        buffer = new ComputeBuffer(Particles.Length, 68);
+        particleBuffer = new ComputeBuffer(Particles.Length, 68);
+        boundaryBuffer = new ComputeBuffer(boundaryParticles.Length, 68);
 
-        buffer.SetData(Particles);
+        particleBuffer.SetData(Particles);
+        boundaryBuffer.SetData(boundaryParticles);
 
         calcVariablesKernel = computeShader.FindKernel("CalculateVariables");
         calcForcesKernel = computeShader.FindKernel("CalculateForces");
         calcPosition = computeShader.FindKernel("CalculatePosition");
 
         computeShader.SetFloat("pi", 3.1415f);
-        computeShader.SetFloat("mass", 80f); // 0.004
-        computeShader.SetFloat("radius", 1.1f); // 0.1
-        computeShader.SetFloat("radius2", 1.21f); 
-        computeShader.SetFloat("gasConstant", 500f); // 2
-        computeShader.SetFloat("restDensity", 1f); // 1
-        computeShader.SetFloat("fluidResistance", 0.1f); // 0.001
+        computeShader.SetFloat("mass", 1f);
+        computeShader.SetFloat("radius", 2f);
+        computeShader.SetFloat("radius2", 4f); 
+        computeShader.SetFloat("gasConstant", 20f);
+        computeShader.SetFloat("exponent", 7f);
+        computeShader.SetFloat("restDensity", 0.35f);
+        computeShader.SetFloat("fluidResistance", 0.1f);
         computeShader.SetFloat("gravity", -9.81f);
-        computeShader.SetFloat("timeStep", 0.009f); // 0.007
+        computeShader.SetFloat("timeStep", 0.01f);
         computeShader.SetFloat("friction", 0.99f);
         computeShader.SetInt("wParticleCount", wCountInt);
-        computeShader.SetInt("allParticlesCount", Particles.Length); 
+        computeShader.SetInt("bParticleCount", bCountInt);
+        computeShader.SetInt("allParticlesCount", Particles.Length + boundaryParticles.Length); 
 
-        computeShader.SetBuffer(calcVariablesKernel, "Particles",buffer);
-        computeShader.SetBuffer(calcForcesKernel, "Particles", buffer);
-        computeShader.SetBuffer(calcPosition, "Particles", buffer);
+        computeShader.SetBuffer(calcVariablesKernel, "Particles",particleBuffer);
+        computeShader.SetBuffer(calcForcesKernel, "Particles", particleBuffer);
+        computeShader.SetBuffer(calcPosition, "Particles", particleBuffer);
+
+        computeShader.SetBuffer(calcVariablesKernel, "boundaryParticles", boundaryBuffer);
+        computeShader.SetBuffer(calcForcesKernel, "boundaryParticles", boundaryBuffer);
+        computeShader.SetBuffer(calcPosition, "boundaryParticles", boundaryBuffer);
     }
 
-    void RenderParticles(int stop)
+    void RenderParticlesUpdate()
     {
-        for (int i = 0; i < stop; i++)
+        for (int i = 0; i < Particles.Length; i++)
         {
-            renderedParticles[i].transform.position = Particles[i].position;
+            renderedFluidParticles[i].transform.position = Particles[i].position;
+        }
+    }
+
+    void RenderBoundaryParticlesUpdate()
+    {
+        for (int i = 0; i < boundaryParticles.Length; i++)
+        {
+            boundaryParticles[i].position = renderedBoundaryParticles[i].transform.position;
         }
     }
 }
